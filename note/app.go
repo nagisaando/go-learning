167beae98205c8cf751e6ff8ede10a36318cb56a
Nagisa Ando <amnot.nobig.deal@gmail.com>
2024-10-11 08:28:30 -0500

    use bufio

--- note/app.go
+++ note/app.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	note_data "example.com/note/note-data"
 )
 
 func main() {
 
-	var title string
-	var content string
-
-	createNoteData("enter title", &title)
-	createNoteData("enter content", &content)
+	title := createNoteData("enter title")
+	content := createNoteData("enter content")
 
 	note, err := note_data.New(title, content)
 
@@ -30,7 +30,23 @@ func main() {
 
 }
 
-func createNoteData(outputText string, data *string) {
+func createNoteData(outputText string) string {
 	fmt.Println(outputText)
-	fmt.Scanln(data)
+	// fmt.Scanln(data) // Scanln() works only single word or number (does not work with space)
+
+	// bufio works to read long text
+	reader := bufio.NewReader(os.Stdin) // making struct and targeting command line to scan text
+
+	// '\n' => this means when user hit "enter" key, it stop scanning text
+	// '\n' => to specify single byte, single character, needs to be wrapped with single quotes
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r") // for windows since return key return as \r\n
+
+	return text
 }
