9a2faa4151ec6cebabaeceef87a35e538a3393f6
Nagisa Ando <amnot.nobig.deal@gmail.com>
2024-11-06 08:23:28 -0500

    fetch event through sqlite

--- rest-api-2/app.go
+++ rest-api-2/app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/rest-api-2/db"
 	"example.com/rest-api-2/models"
@@ -13,16 +14,39 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/events", getEvents)
+	router.GET("/events/:id", getEventById)
 	router.POST("/events", createEvent)
 
 	router.Run("localhost:8080")
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Could not fetch events. Try again later."})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
+func getEventById(context *gin.Context) {
+	stringId := context.Param("id")
+	id, err := strconv.ParseFloat(stringId, 64)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Could not fetch event. Tey again later"})
+		return
+	}
+	event, err := models.GetEventByID(id)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Could not fetch event. Tey again later"})
+		return
+	}
+
+	// context.JSOn will handle the pointer automatically
+	// hence no need to dereference event beforehand
+	context.JSON(http.StatusOK, event)
+}
 func createEvent(context *gin.Context) {
 	var event models.Event
 
@@ -37,7 +61,11 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create event. Try again later."})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 
--- rest-api-2/models/event.go
+++ rest-api-2/models/event.go
@@ -2,10 +2,12 @@ package models
 
 import (
 	"time"
+
+	"example.com/rest-api-2/db"
 )
 
 type Event struct {
-	ID          int       `json:"id"`
+	ID          int64     `json:"id"`
 	Name        string    `json:"name" binding:"required"`
 	Description string    `json:"description" binding:"required"`
 	Location    string    `json:"location" binding:"required"`
@@ -24,11 +26,107 @@ type Event struct {
 
 var events = []Event{}
 
-func (event Event) Save() {
-	// [TODO]: add it to database
-	events = append(events, event)
+func (event *Event) Save() error {
+
+	// ? is parameter in SQLite
+	query := `
+	INSERT INTO events (name, description, location, dateTime, userId)
+	VALUES(?, ?, ?, ?, ?)
+	`
+
+	// Prepare compiles the SQL command but does not execute it immediately. It creates a "template" that can be executed with parameters
+	// it can use the both db.DB.prepare and db.DB.execute() but
+	// Prepare() can lead to better performance in certain situations such as when we want to execute the same query multiple times with different values
+	// UNLESS statement.Close() is not called between the executions. otherwise there is no advantages
+	statement, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close() // this will be executed at the end of Save function
+
+	// Exec()  runs the prepares statement
+	// Exec() is typically used to update a data while Query() is used to read a data
+	result, err := statement.Exec(event.Name, event.Description, event.Location, event.DateTime, event.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId() // get auto generated id
+
+	if err != nil {
+		return err
+	}
+
+	event.ID = id
+	events = append(events, *event)
+
+	return nil
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+
+	// we can query all keys using * but it is recommended for testing purpose only: https://www.sqlitetutorial.net/sqlite-select/
+
+	query := `
+	SELECT
+		id,
+		name,
+		description,
+		location,
+		dateTime,
+		userId
+	FROM
+		events
+	`
+
+	var events []Event
+	rows, err := db.DB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	// if there is any issue during iteration of rows.Next(),
+	// error will be stored in rows.Err()
+	if err = rows.Err(); err != nil {
+		return events, err
+	}
+
+	return events, nil
+}
+
+// returning pointer for memory efficiency
+func GetEventByID(id float64) (*Event, error) {
+
+	// we can query all keys using * but it is recommended for only testing purpose: https://www.sqlitetutorial.net/sqlite-select/
+	query := `SELECT * FROM events WHERE id = ?`
+	// QueryRow() returns only one row
+	// no need to close the connection
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+
 }
